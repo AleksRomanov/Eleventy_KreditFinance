cf94ee271f27c6fe77f87508a710716c15c6f630
aleksromanov <kosmonavt-vavilon@mail.ru>
2022-05-31 10:53:56 +0700

    second commit

---
+++ .eleventy.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const htmlmin = require('html-minifier');
+const markdown = require('markdown-it')({ html: true });
+const music = require('music-metadata');
+const prettydata = require('pretty-data');
+
+module.exports = (config) => {
+    config.addFilter('length', (path) => {
+        const stats = fs.statSync(path);
+        return stats.size;
+    });
+
+    const getDuration = (path) => {
+        return music.parseFile(path)
+            .then(metadata => {
+                const duration = parseFloat(metadata.format.duration);
+                return new Date(Math.ceil(duration) * 1000).toISOString().substr(11, 8);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    config.addNunjucksAsyncFilter('duration', async (path, callback) => {
+        const duration = await getDuration(path);
+
+        callback(null, duration);
+    });
+
+    config.addFilter('ruDate', (value) => {
+        return value.toLocaleString('ru', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }).replace(' г.', '');
+    });
+
+    config.addFilter('enDate', (value) => {
+        return value.toLocaleString('en', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+    });
+
+    config.addFilter('htmlmin', (value) => {
+        return htmlmin.minify(
+            value, {
+                removeComments: true,
+                collapseWhitespace: true
+            }
+        );
+    });
+
+    config.addFilter('markdown', (value) => {
+        return markdown.render(value);
+    });
+
+    config.addTransform('xmlmin', (content, outputPath) => {
+        if(outputPath && outputPath.endsWith('.xml')) {
+            return prettydata.pd.xmlmin(content);
+        }
+        return content;
+    });
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist',
+            includes: 'includes',
+            data: 'data'
+        },
+        dataTemplateEngine: 'njk',
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk'
+    };
+};
